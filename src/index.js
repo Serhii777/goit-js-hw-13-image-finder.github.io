e432bef34032adf9afe99aa741e2e21bd8a5c367
Serhii777 <muzykasv72@gmail.com>
2020-08-17 13:23:59 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './js/pixabay-search';
-import './js/services/pixabayAsync-services';
-import './js/spinner';
-import './js/buttonScrollTop';
-import './js/services/notices';
-import './js/gallery-item';
-
-import './sass/main.scss';
-
