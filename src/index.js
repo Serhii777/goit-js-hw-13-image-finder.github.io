a2ce0cbe664da1a9539bc638359bffe05cb7aa39
Serhii777 <muzykasv72@gmail.com>
2020-08-17 13:16:59 +0300

    Build vebpack

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import './js/pixabay-search';
 import './js/services/pixabayAsync-services';
 import './js/spinner';
 import './js/buttonScrollTop';
-// import './js/scrollingByKeys';
 import './js/services/notices';
 import './js/gallery-item';
-// import './js/infinite-scroll'
 
 import './sass/main.scss';
 
